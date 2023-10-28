e41f39c1056ff7e9e72f68d4cf6ef789f277eb6b
Jonathan Johnson <95863021+johnnyd81@users.noreply.github.com>
2023-10-28 09:41:01 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+//import the components needed to render on the screen
 import Header from './Components/Header';
 import LandingPage from './Components/LandingPage';
 import Product from './Components/Product';
@@ -8,6 +9,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 
 const loggedIn = true;
+//the products array contains objects that has data about the different food dishes 
 const products = [{id: "1", product_name: "Chicken Tacos", product_img: "https://cdn.wallpapersafari.com/57/62/dcHCIS.jpg", description: 
 "Deliciously prepared chicken wrapped in our soft corn tacos. Each plate comes with four 4-inch tacos for R65.00" },
 {id: "2", product_name: "Burritos", product_img: "https://th.bing.com/th/id/OIP.1U7lr9Ga76_baUE2jxHTXAHaFj?pid=ImgDet&rs=1", description: 
